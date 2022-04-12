934d50fa07b23bddcb977f14c4f74b99d34e8b03
NeAlien <goza.sg0@gmail.com>
2022-04-12 04:25:11 +0300

    Update Hello world.cpp

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -11,8 +11,8 @@ main(int argc, char** argv)
 =======
 
 int main(int argc, char** argv){
- std:: string name; // User name
- std::cin >> name; // Input user name
+ std:: string name; // User n.
+ std::cin >> name; // Ввод данных
  std::cout << "Hello world from " << name << std::endl;
 >>>>>>> 5ad54944196ce5781d042908cfa0648035cbc950
 }
